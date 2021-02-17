f621bdf5d5b72210d1c5fdfecb2b24d599eb5d4a
cxw320 <cxw320@case.edu>
2021-02-17 18:47:46 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 8055;
+
+app.get('/ping', (req,res)=> res.send('pong'));
+
+
+app.get('/heartbeat', (req,res)=> res.send({datetime: new Date().toJSON()}));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
+
