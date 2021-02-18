0d58fbc871ae43e6dc53e01cf3fc63fc6d5816e2
cxw320 <cxw320@case.edu>
2021-02-17 19:12:40 -0500

    add User

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 import express from 'express';
+import UserModel from './User';
+
+const user1 = new UserModel({
+    id: '123',
+    usernam: 'jondoe',
+    email: 'jondo@email.com',
+    role: 'admin',
+});
+
+const user2 = new UserModel({
+    id: '345',
+    username: 'janedoe',
+    email: 'janedoe@email.com',
+    role: 'customer',
+});
+
+const users = [user1.getData(), user2.getData()];
+console.log(users);
+
+
 
 const app = express();
 const port = 8055;
@@ -11,3 +31,4 @@ app.get('/heartbeat', (req,res)=> res.send({datetime: new Date().toJSON()}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
 
+//middleware in Express
