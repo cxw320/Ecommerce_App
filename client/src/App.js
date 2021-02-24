fbb39ca8a5b36313ada24876d65a42772ae09651
cxw320 <cxw320@case.edu>
2021-02-23 20:05:47 -0500

    Product Card

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import DynamicCounter from './components/DynamicCounter.js';
+import { render, screen } from '@testing-library/react';
 
-function App() {
+import React, {Component} from 'react';
+import ProductCard from './components/ProductCard';
+
+const images = [
+  'https://cdn.shopify.com/s/files/1/0204/7208/products/IMG_2111_c1fe0803-f19c-4971-919d-62eeb71ed3bf_620x.jpg?v=1574787075',
+  'https://cdn.shopify.com/s/files/1/0204/7208/products/IMG_2125_e3da5891-98e2-461a-9d94-788dcac1b7c8.jpg?v=1574787075'
+      
+];
+
+class App extends Component {
    // state lifting, bringing the state from children to parent component
-  
+  render(){
 
-  return (
-    <div className="App">
-      <div className="App-header">
-      <DynamicCounter value={'Apples'} increment={this.increment} decrement={this.decrement}/>
-      <DynamicCounter value={'Oranges'} increment={this.increment} decrement={this.decrement}/>
-      <DynamicCounter value={'Lemons'} increment={this.increment} decrement={this.decrement}/>
-      </div>
-    </div>
-  );
+      return (
+        <div className="App">
+
+          <ProductCard 
+            images={images}
+            name="Product XYZ"
+            price="49.99"/>
+
+        </div>
+      );
+  }
 }
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import React, {Component} from 'react';
+import ProductCard from './components/ProductCard';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+/*
+class App extends Component {
+   // state lifting, bringing the state from children to parent component
+  render(){
+
+      return (
+        <div className="App">
+
+          <ProductCard />
+
+        </div>
+      );
+  }
+}
+
+export default App;*/
