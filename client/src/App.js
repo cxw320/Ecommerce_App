67e74a3196bd1a7765e360abec4100eb48f2213e
cxw320 <cxw320@case.edu>
2021-03-04 21:50:33 -0500

    Update routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import React, {Component} from 'react';
-import ProductCard from './components/product/ProductCard';
-import products from './data/products.js';
-import ProductList from './components/product/ProductList';
-import './App.css';
-import BaseInput from './components/inputs/BaseInput.js';
-import TextInput from './components/inputs/TextInput';
-import PasswordInput from './components/inputs/PasswordInput';
-import CheckboxInput from './components/inputs/CheckboxInput';
-import Form from './components/Form.js';
-import {PrimaryButton, SecondaryButton} from './components/Button.js';
-import{
+import React, { Component } from 'react';
+import store from 'store2';
+import NavigationBar from './components/NavigationBar';
+import {
   BrowserRouter as Router,
   Switch,
   Route,
@@ -19,21 +10,111 @@ import './App.css';
 import Home from './pages/Home';
 import FormDemo from './pages/FormDemo';
 import NotFound from './pages/NotFound';
+import Category from './pages/Category';
+import Product from './pages/Product';
+import Orders from './pages/Orders';
+import Cart from './pages/Cart';
+import Account from './pages/Account';
+import UserManagement from './pages/admin/UserManagement';
+import ProductManagement from './pages/admin/ProductManagement';
+import Auth from './pages/Auth';
+import {getCurrentUser} from './api/Auth';
 
 class App extends Component {
-  render(){
-    return (
-        <div className="App">
-            <Router>
-                <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/forms" exact component={FormDemo} />
-                    <Route component={NotFound}/>
-                </Switch>
-            </Router>
-        </div>
-    )
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemsInCart: store.get('itemsInCart') || [],
+      user: undefined,
+    };
+    this.ProductPage = Product(this.addToCart);
+  }
 
+  componentDidMount = () => {
+    this.authUser();
+    document.addEventListener(
+      'visibilitychange',
+      () => {
+        if (!document.hidden) {
+          this.setState({
+            itemsInCart: store.get('itemsInCart') || []
+          });
+          this.authUser();
+        }
+      }
+    );
+  };
+
+  authUser = async () => {
+    const result = await getCurrentUser();
+    if (result && result.data) {
+      this.setState({ user: result.data })
+    } else {
+      this.setState({ user: undefined });
+    }
+  };
+
+  addToCart = (item) => {
+    const { itemsInCart } = this.state;
+    itemsInCart.push(item);
+    this.setState({ itemsInCart });
+    store.set('itemsInCart', itemsInCart);
+  };
+
+  removeFromCart = (index) => {
+    const { itemsInCart } = this.state;
+    itemsInCart.splice(index, 1);
+    this.setState({ itemsInCart });
+    store.set('itemsInCart', itemsInCart);
+  };
+
+  emptyCart = () => {
+    this.setState({ itemsInCart: [] });
+    store.set('itemsInCart', []);
+  };
+
+  render() {
+    const isLoggedIn = this.state.user && this.state.user._id;
+    return (
+      <Router>
+        <div className="App">
+          <NavigationBar
+            isLoggedIn={isLoggedIn}
+            itemsInCart={this.state.itemsInCart.length}
+          />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/auth/:token" exact component={Auth(this.authUser)} />
+            <Route path="/forms" exact component={FormDemo} />
+            <Route
+              path="/cart"
+              exact
+              render={props =>
+                <Cart
+                  {...props}
+                  items={this.state.itemsInCart}
+                  removeFromCart={this.removeFromCart}
+                  emptyCart={this.emptyCart}
+                />
+              }
+            />
+            <Route path="/orders" exact component={Orders} />
+            <Route path="/account" exact component={Account} />
+            <Route path="/category/:slug" component={Category} />
+            <Route path="/product/:id" component={this.ProductPage} />
+
+            {isLoggedIn && this.state.user.role === 'admin' &&
+              <Route path="/admin/users" exact component={UserManagement} />}
+            {isLoggedIn && this.state.user.role === 'admin' &&
+              <Route path="/admin/products" exact component={ProductManagement} />}
+
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
+
+    );
+  }
 }
-}
+
 export default App;
